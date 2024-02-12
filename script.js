1ec14b672ba63a48b9d23820a57502ac925507a3
mmustapha <mustaphamahmoud83@gmail.com>
2024-02-12 02:49:08 +0200

    bugfixes & design re-done

--- script.js
+++ script.js
@@ -1,39 +1,92 @@
 "use strict";
-const sketch = document.getElementById("grid");
-const slider = document.querySelector(".slider");
-const output = document.getElementsByTagName("label")[0];
-const grid = document.getElementById("grid");
-createGrid(Number(slider.value));
 
+const GRID = document.getElementById("grid"); //drawing area
+const PALLETE = document.getElementById("color-picker") // color picker
+const COLOR_OUT = document.getElementById("color-indicator") // color indicator txt
+const SIZER = document.querySelector(".sizer"); // size setter
+const SIZE_OUT = document.getElementById("size-indicator") //size indicator txt
+const RESTART = document.getElementById("reset"); //reset button
+const FOOTER = document.getElementById("footer") // footer
+
+const DEFAULT_SIZE = 30;
+const DEFAULT_COLOR = "Black";
+
+// automatic year updates for footer.
+let date = new Date()
+let year = date.getFullYear()
+FOOTER.innerHTML = FOOTER.textContent + year
+
+
+// reset button action
+RESTART.addEventListener('click', reset)
+
+// color picker action
+PALLETE.addEventListener('input', ()=>{
+  COLOR_OUT.innerHTML = 'Color: ' + PALLETE.value})
+
+//size buttin action
+SIZER.addEventListener('input', ()=>{
+  createGrid(Number(SIZER.value));
+} )
+
+
+//create default-setting grid
+createGrid(Number(SIZER.value));
+
+// detect mouse click  -- TODO
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+document.body.onmousedown = () => {mouseDown = true};
+document.body.onmouseup = () => {mouseDown = false};
 
 function createGrid(value) {
-  grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
-  grid.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+  // this function creates a grid basd on chosen size
+  GRID.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  GRID.style.gridTemplateRows = `repeat(${value}, 1fr)`;
 
+  // create a square grid with size*size dimensions
   for (let i = 0; i < value * value; i++) {
     let newDiv = document.createElement("div");
+    newDiv.style.border = '.1px solid rgba(112,112,112,0.05)';
     newDiv.addEventListener("mouseover", paint);
     newDiv.addEventListener("mousedown", paint);
-    grid.appendChild(newDiv);
+    GRID.appendChild(newDiv);
   }
 }
+
 
 function paint(e) {
+// apply set color to pixels pointeed at by mouse & click or erasing a pixel with ctrl+click
   if (e.type === "mouseover" && !mouseDown) return;
-  if (true) e.target.style.backgroundColor = "black";
-}
-
-slider.oninput = function () {
-  clearGrid();
-  output.innerHTML = this.value;
-  createGrid(this.value);
-};
-
-function clearGrid() {
-  for (let j = sketch.childElementCount; j > 0; j--) {
-    sketch.removeChild(sketch.firstChild);
+  if (e.type === "mouseover"  && e.ctrlKey || e.type === "mousedown"  && e.ctrlKey) {
+    e.target.style.backgroundColor = 'whitesmoke';
+  } else {
+    let color = document.getElementById("color-picker").value;
+    e.target.style.backgroundColor = color;
   }
 }
+
+
+// clean the grid from paint.
+function clearGrid() {
+  for (let j = GRID.childElementCount; j > 0; j--) {
+    GRID.removeChild(GRID.firstChild);
+  }
+}
+
+
+function reset() {
+  // reset the grid and settings to defaults
+  clearGrid();
+  SIZER.value =  DEFAULT_SIZE;
+  SIZE_OUT.innerHTML = "Size: " +  30;
+  PALLETE.value = '#000';
+  COLOR_OUT.innerHTML = "Color: " + "Black" 
+  createGrid(Number(SIZER.value));
+}
+
+// rebuild the grid after size change
+SIZER.oninput = function () {
+  clearGrid();
+  SIZE_OUT.innerHTML = "Size: "+ this.value;
+  createGrid(this.value);
+};
